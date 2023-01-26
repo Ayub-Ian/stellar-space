b96723b7812d547f2f6fa5c89128d66360c716d4
dancun616 <dancun616@gmail.com>
2023-01-27 01:20:33 +0300

    weather compopnent

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,14 @@
 
 import HomePage from '../homepage/HomePage';
 import './App.css';
+import AboutMars from '../weather/AboutMars';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
       <h1>Welcome to, Stellar Space </h1>
+      <AboutMars/>
     </div>
   );
 }
