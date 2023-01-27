21a2c18da5c0956212b63d5009a45fe57ee16f64
Ayub Ian <iayub.mia@gmail.com>
2023-01-27 07:18:13 +0300

    Added filter on mars rovers photos

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,46 +1,41 @@
-import React, { useEffect,useState } from 'react'
-import "./HomePage.css"
+import React, { useEffect, useState } from "react";
+import "./HomePage.css";
 
 function HomePage() {
-  const [data,setData] = useState([])
-  const url = 'https://api.nasa.gov/planetary/apod?api_key=FaM2S61TschjDYQGiFUfpUdDFZz3Be0BZJWDUzeu'
-  useEffect(()=>{ 
+  const [data, setData] = useState([]);
+  const url =
+    "https://api.nasa.gov/planetary/apod?api_key=FaM2S61TschjDYQGiFUfpUdDFZz3Be0BZJWDUzeu";
+  useEffect(() => {
     fetch(url)
-    .then(res=>res.json())
-    .then(data=>{
-      setData(data)
-      console.log(data) 
-    })
-  },[])
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
   return (
     <>
-     <div className='home'>
-        <div className='container'>
-        <h5>Welcome to, Stellar Space </h5>
-        <div className="phrase">
-        <h5 className='margin'>EXPLORE THE</h5>
-        <h5>EXTRAORDINARY, </h5>
-        <h5>EVERYDAY </h5>
+      <div className="home">
+        <div className="container">
+          <h5>Welcome to, Stellar Space </h5>
+          <div className="phrase">
+            <h5 className="margin">EXPLORE THE</h5>
+            <h5>EXTRAORDINARY, </h5>
+            <h5>EVERYDAY </h5>
+          </div>
         </div>
-        </div>
-    </div>
-   
-      {
-          <>
-            <div className="first">
-              <p>Title : {data.title}</p>
-              <p>Date : {data.date}</p>
-              <h4>Explanation</h4>
-              <p>{data.explanation}</p>
-            </div>
-            <h4 className='pod'>Picture of the day</h4>
-            <img className='image' src={data.hdurl} alt='' />
-          </>
-      } 
+      </div>
+
+      <div className="first">
+        <p>Title : {data.title}</p>
+        <p>Date : {data.date}</p>
+        <h4>Explanation</h4>
+        <p>{data.explanation}</p>
+      </div>
+      <h4 className="pod">Picture of the day</h4>
+      <img className="image" src={data.hdurl} alt="picture of the day" />
     </>
-   
-    
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
--- src/components/mars-rovers-photos/MarsRoversPhotos.js
+++ src/components/mars-rovers-photos/MarsRoversPhotos.js
@@ -5,25 +5,40 @@ import "./RoverPhotos.css";
 
 function MarsRoversPhotos() {
 
+  let API_URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-6-3&page=1&camera=fhaz&api_key=fSMOajnpgriQhDldxqKguMGni5blka3Cfy5CUGY1'
   const [photos, setPhotos] = useState([])
+  const [api, setApi] = useState(API_URL)
+  
+  
+
   useEffect(() => {
-   fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-6-3&page=1&camera=fhaz&api_key=fSMOajnpgriQhDldxqKguMGni5blka3Cfy5CUGY1')
+   fetch(api)
    .then(res => res.json())
    .then(photos => setPhotos(photos.photos))
-  }, [])
+  }, [api])
+
+  function onFilter(data) {
+    console.log(data)
+    if (data.pov === "all") {
+      setApi(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${data.date}&page=1&api_key=fSMOajnpgriQhDldxqKguMGni5blka3Cfy5CUGY1`)
+    } else {
+     setApi(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${data.date}&page=1&camera=${data.pov}&api_key=fSMOajnpgriQhDldxqKguMGni5blka3Cfy5CUGY1`)
+    }
+  }
+  
   
 
   return (
     <div className="mars-photos">
       <div className="mp-wrapper container">
         <h1 className="mp-title">Mars Rovers Photos</h1>
-        <p class="mp-intro">
+        <p className="mp-intro">
           On this website you can find photos of rovers on Mars. It uses NASA
           Apis to show you all the details about the rovers, including their
           images. Some dates has not results so try one day before or after. Use
           the following options to filter your search.
         </p>
-        <FilterPhotos />
+        <FilterPhotos onFilter={onFilter} />
         <PhotoList photos={photos} />
       </div>
     </div>
